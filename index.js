7190093ca911a819bf6e6f613820d338ed9bc352
YBJ0000 <95595902+YBJ0000@users.noreply.github.com>
2024-10-13 14:35:13 +1100

    js dom 1

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 import './index.css';
+import { sidebar } from "./data/db.json"
+
+const mainLeft = document.querySelector('.main-left')
+mainLeft.innerHTML = ""
+
+const sideBar = document.createElement('div')
+sideBar.classList.add('sidebar')
+mainLeft.appendChild(sideBar)
+
+console.log(mainLeft);
+
+sidebar.forEach((item) => {
+  const sidebarItem = document.createElement('a')
+  sidebarItem.classList.add('menu-item')
+  const span = document.createElement('span')
+  const i = document.createElement('i')
+  item.icon_class_list.forEach(iconClass => {
+    i.classList.add(iconClass)
+  })
+  const h2 = document.createElement('h2')
+  h2.textContent = item.name
+  
+  span.appendChild(i)
+  sidebarItem.appendChild(span)
+  sidebarItem.appendChild(h2)
+
+  sideBar.appendChild(sidebarItem)
+})
+
