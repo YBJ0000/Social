7800ae00a18abdb2e61d5f7586bf66649a3fe699
YBJ0000 <95595902+YBJ0000@users.noreply.github.com>
2024-10-18 10:37:48 +1100

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ mainLeft.appendChild(sideBar)
 console.log(mainLeft);
 
 sidebar.forEach((item) => {
+  createSideBarItem(item)
+})
+
+const homeDom = document.querySelector('.Home')
+console.log(homeDom);
+
+homeDom.addEventListener('click', () => {
+  homeDom.classList.toggle('btn')
+  homeDom.classList.toggle('btn-primary')
+})
+
+function createSideBarItem(item) {
   const sidebarItem = document.createElement('a')
   sidebarItem.classList.add('menu-item')
   const span = document.createElement('span')
@@ -26,13 +38,4 @@ sidebar.forEach((item) => {
   sidebarItem.appendChild(span)
   sidebarItem.appendChild(h2)
   sideBar.appendChild(sidebarItem)
-})
-
-const homeDom = document.querySelector('.Home')
-console.log(homeDom);
-
-homeDom.addEventListener('click', () => {
-  homeDom.classList.toggle('btn')
-  homeDom.classList.toggle('btn-primary')
-})
-
+}
